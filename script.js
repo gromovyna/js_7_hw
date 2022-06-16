2be17265a1a6083e62c0f038c724b33455056482
Влад <vlad@MacBook-Pro-Vlad.local>
2022-06-16 09:32:24 +0300

    add new

---
+++ script.js
@@ -0,0 +1,30 @@
+sports = [
+    ['skier','⛷'],
+    ['snowboarder','🏂'],
+    ['apple','🍎'],
+    ['hockey','🏒'],
+    ['ice skate','⛸'],
+    ['swimmer','🏊'],
+    ['surfer','🏄‍'],
+    ['watermelon','🍉'],
+    ['lemon','🍋'],
+    ['rowboat','🚣'],
+    ['bicyclist','🚴‍']
+   ];
+   fruits = [];
+
+   summer_sports = sports.slice([5],[11]); 
+
+   winter_sports = sports.slice([0],[5]);  
+
+   fruits.push(summer_sports.splice(2,2));
+
+   fruits.push(winter_sports.splice(2,1));
+   
+   console.log(summer_sports);
+   console.log(winter_sports);
+   console.log(fruits);
+
+  
+
+
